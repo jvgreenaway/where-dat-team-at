35af77df24381b992318cee87829b90836c96c11
James Greenaway <james@100shapes.com>
2017-05-12 17:39:24 +0100

    c

--- server.js
+++ server.js
@@ -5,8 +5,34 @@ const ConvoStore = require('slapp-convo-beepboop')
 const Context = require('slapp-context-beepboop')
 
 const port = process.env.PORT || 3000
+const token = process.env.SLACK_TOKEN
 
-console.log(process.env.SLACK_TOKEN)
+const memberNames = {
+  U1CTUKMT9: 'Zoé',
+  U40MZ9EL9: 'Tugba',
+  U0BU8V7N0: 'Trev',
+  U2NC0H2BS: 'Temi',
+  U0BUEB5BK: 'Paolo',
+  U0BU8SV4K: 'Michele',
+  U1KU01R4K: 'Kristina',
+  U162CB0LC: 'James',
+  U0C160N8N: 'Christean',
+  U0BUPA30R: 'Chris',
+  U1CU5P4H0: 'Anna',
+}
+
+const members = Object.keys(memberNames)
+
+
+slack.users.list({ token }, (err, data) => {
+  if (err) throw new Error(err)
+  console.log('users')
+  console.log(data)
+})
+
+
+
+
 
 const slapp = Slapp({
   verify_token: process.env.SLACK_VERIFY_TOKEN,
